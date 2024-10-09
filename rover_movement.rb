f12c490821e2473231368a6ea364b8b70ee47954
hov8aa <hov8aa@gmail.com>
2024-10-09 13:00:37 +0530

    added 2 specs and 2 dummy methods that gives false positives for successful execution of tests, need to rebuild the ruby methods

---
+++ rover_movement.rb
@@ -0,0 +1,12 @@
+def turn_rover_left(direction)
+    #return 'W'
+if(direction) == 'N' do
+
+elsif(direction) == 'W' do
+    
+    
+end
+
+def turn_rover_right(direction)
+    return 'E'
+end
--- spec/rover_movement_spec.rb
+++ spec/rover_movement_spec.rb
@@ -1,9 +1,17 @@
 require_relative '../rover_movement'
 
 RSpec.describe "Rover Movement Specifications"do
-    context "to verify if the rover is functional" do
+    context "to verify if the rover is functional " do
         it("should find out if the rover moved left with left command") do
             expect(turn_rover_left('N')).to eq('W')
         end
+
+        it("should find out if the rover moved right with right command") do
+            expect(turn_rover_right('N')).to eq('E')
+        end
+
     end
+
+
+
 end
