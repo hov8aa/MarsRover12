bd6f0d94a686727c79e4ce05738ed3cd08a09170
hov8aa <hov8aa@gmail.com>
2024-10-09 13:09:04 +0530

    added 2 specs and 2 dummy methods that gives false positives for successful execution of tests, need to rebuild the ruby methods & added a default ruby action to git repo

--- rover_movement.rb
+++ rover_movement.rb
@@ -2,7 +2,7 @@ def turn_rover_left(direction)
     if(direction) == 'N'
         return 'W'
     elsif(direction) == 'W'
-    
+        return 'S'
     elsif(direction) == 'S'
 
     elsif(direction) == 'E'
