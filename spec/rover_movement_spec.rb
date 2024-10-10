c16ca0b5658c6b5997575eb4b0d685aa55e76790
hov8aa <hov8aa@gmail.com>
2024-10-10 14:04:28 +0530

    Updating workflow to support a specific ubuntu version somehow worked, I couldn't understand why. Anyway, moving forward to enable my latest additions to have my first intentional red build. In commit #10, I missed to verify why the commit failed before commiting my #11

--- spec/rover_movement_spec.rb
+++ spec/rover_movement_spec.rb
@@ -10,17 +10,15 @@ RSpec.describe "Rover Movement Specifications"do
             expect(turn_rover_right('N')).to eq('E')
         end
 
-    #    it("should validate the new position of the face of the rover, after moving one step forward") do
-    #        expect(move_rover_forward('x','y')).to eq('x,y+1')
-    #    end
+        it("should validate the new position of the face of the rover, after moving one step forward") do
+            expect(move_rover_forward('x','y')).to eq('x,y+1')
+        end
     end
 
-=begin
     context"to verify if the rover moves as expected when there are a series of instructions"do
         it("should validate the ") do
             expect('LMLMLMLMM').to eq('N')
         end
     end
-=end
 
 end
