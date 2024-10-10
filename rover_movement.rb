be7d0ea745d99c8399b0d674999a064304b5714a
hov8aa <hov8aa@gmail.com>
2024-10-10 12:21:53 +0530

    rewrote the it block for the initial 2 specs to make better sense of the autogenerated documentation through rspec and added my first intentional red build by adding a spec & not adding it's related code.

--- rover_movement.rb
+++ rover_movement.rb
@@ -4,13 +4,20 @@ def turn_rover_left(direction)
     elsif(direction) == 'W'
         return 'S'
     elsif(direction) == 'S'
-
+        return 'E'
     elsif(direction) == 'E'
-
+        return 'N'
     end
-    
 end
 
 def turn_rover_right(direction)
-    return 'E'
+    if(direction) == 'N'
+        return 'E'
+    elsif(direction) == 'E'
+        return 'S'
+    elsif(direction) == 'S'
+        return 'W'
+    elsif(direction) == 'W'
+        return 'N'
+    end
 end
--- spec/rover_movement_spec.rb
+++ spec/rover_movement_spec.rb
@@ -2,16 +2,16 @@ require_relative '../rover_movement'
 
 RSpec.describe "Rover Movement Specifications"do
     context "to verify if the rover is functional " do
-        it("should find out if the rover moved left with left command") do
+        it("should find out the new direction of the face of the rover, after terning left") do
             expect(turn_rover_left('N')).to eq('W')
         end
 
-        it("should find out if the rover moved right with right command") do
+        it("should find out the new direction of the face of the rover, after terning right") do
             expect(turn_rover_right('N')).to eq('E')
         end
 
+        it("should find out the new rover position after moving one step forward") do
+            expect(move_rover_forward('x','y')).to eq('x,y+1')
+        end
     end
-
-
-
 end
